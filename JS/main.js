60916f6885f59b814e5a82e51e9d6a52a6db8c26
Alessandro <andro.mieli2001@gmail.com>
2020-04-02 00:14:10 +0200

    Initial commit
    Creating the web page navbar

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+// 	$('#nav-icon').click(function(){
+// 		$(this).toggleClass('open');
+// 	});
+// });
+
+function openNav() {
+    document.getElementById("navbarSupportedContent").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("navbarSupportedContent").style.width = "0";
+  }
