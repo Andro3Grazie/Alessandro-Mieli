6639db9b2b33341f6d48d0a25edb42b76a33979d
Alessandro <andro.mieli2001@gmail.com>
2020-04-09 15:49:38 +0200

    Footer
    adding the footer with relatives links and copyright name of the page

--- script/functions.js
+++ script/functions.js
@@ -1,6 +1,20 @@
-function openNav() {
+function openNav() { // Open navbar on mobile
     document.getElementById("navbarSupportedContent").style.width = "100%";
 }
-function closeNav() {
+function closeNav() { // Close navbar on mobile
     document.getElementById("navbarSupportedContent").style.width = "0";
 }
+$(document).ready(function() { // Change color on contact link hover
+    $('#contact-item-a').hover(function(){     
+        $('#contact-item-b').toggleClass('contact-item-hover');
+        $('#contact-item-c').toggleClass('contact-item-hover');  
+    }),
+    $('#contact-item-b').hover(function(){     
+        $('#contact-item-a').toggleClass('contact-item-hover');
+        $('#contact-item-c').toggleClass('contact-item-hover');  
+    }),
+    $('#contact-item-c').hover(function(){     
+        $('#contact-item-a').toggleClass('contact-item-hover');
+        $('#contact-item-b').toggleClass('contact-item-hover');  
+    });
+});
